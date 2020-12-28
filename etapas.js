94ef0ff16e801cccb768474e5e63362760c34cfe
Suzana Suzuki <s.dasilvasuzuki@gmail.com>
2020-12-28 16:20:18 -0300

    alteracaoJS

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'131422.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'602230.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'13.jpg', legenda: 'Prefeito'},
+                    {url:'22.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos:[
+                    {url:'60.jpg', legenda: 'Prefeito'},
+                    {url:'602230.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
