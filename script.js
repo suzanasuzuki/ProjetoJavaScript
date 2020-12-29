5a7b941269bcb80a68005ba75c3541087df55cb4
Suzana Suzuki <s.dasilvasuzuki@gmail.com>
2020-12-29 15:49:35 -0300

    alteracoesJS2

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ let seuVotoPara = document.querySelector('.d-1-1 span');
 let cargo = document.querySelector('.d-1-2 span');
 let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
-let lateral = document. querySelector('.d-1-right');
-let numeros = documento. querySelector('.d-1-3');
+let lateral = document.querySelector('.d-1--right');
+let numeros = document. querySelector('.d-1-3');
 
 let etapaAtual = 0;
+let numero = '';
 
 function comecarEtapa() {
-    let etapa = etapa[etapaAtual];
-
+    let etapa = etapas[etapaAtual];
+    
     let numeroHtml = '';
+
+    for(let i=0;i<etapa.numeros;i++) {
+        if(i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
 
     seuVotoPara.style.display = 'none';
     cargo.innerHTML = etapa.titulo;
@@ -18,19 +27,51 @@ function comecarEtapa() {
     aviso.style.display = 'none';
     lateral.innerHTML = '';
     numeros.innerHTML = numeroHtml;
+
 }
-  
+
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=> {
+        if(item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    if(candidato.length > 0) {
+        candidato = candidato[0];
+    }
+}
+
 
 function clicou(n) {
-    alert("Clicou em " +n);
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero= `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            atualizaInterface();
+        }
+        
+    }
 }
 
 function branco() {
     alert("Clicou em BRANCO!");
 }
+
 function corrige() {
     alert("Clicou em CORRIGE!");
 }
+
 function confirma() {
     alert("Clicou em CONFIRMA!");
 }
+
+comecarEtapa();
